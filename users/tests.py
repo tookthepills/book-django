d929e508de087688a588585a8f2fcedd03137bdc
tookthepills <pheewooH@gmail.com>
2023-05-23 17:17:32 +0600

    Add CustomUserCreationForm, CustomUserChangeForm Write test for them add db.sqlite3 in .gitignore

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username = 'phee',
+            email = 'pheew@gmail.com',
+            password = 'testpass123'
+        )
+
+        self.assertEqual(user.username, 'phee')
+        self.assertEqual(user.email, 'pheew@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username = 'superuser',
+            email = 'superuser@gmail.com',
+            password = 'testpass123'
+        )
+
+
+        self.assertEqual(user.username, 'superuser')
+        self.assertEqual(user.email, 'superuser@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
